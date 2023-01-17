08f00695864ba0733aee7dabba8fc566603dedd1
rohit <rkg10072000@gmail.com>
2023-01-17 12:23:33 +0530

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+       <RouterProvider router={router} />
     </div>
   );
 }
